376b35a44e08084d107f4b66b3a1603df0fb81e8
Scolon <scoloned@gmail.com>
2026-01-13 17:57:50 -0500

    this works for now

--- Program.cs
+++ Program.cs
@@ -2,60 +2,65 @@
 using System.Text;
 
 
-string executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+string executingDirectory = AppDomain.CurrentDomain.BaseDirectory;
 Console.WriteLine(executingDirectory);
 string[] fileNames = Directory.GetFiles(executingDirectory);
 string expectedFilePath = Path.Combine(executingDirectory, "Content.mgcb");
+Console.WriteLine(expectedFilePath);
 if (!File.Exists(expectedFilePath))
 {
     Console.WriteLine($"Content.mgcb is not in this folder, please put the executable in the same directory as Content.mgcb");
+    Console.ReadLine();
     return;
 }
 
 Console.WriteLine(expectedFilePath);
 
-StringBuilder stringBuilder = new StringBuilder();
+StringBuilder contentBuilder = new StringBuilder();
 //Write default properties
-stringBuilder.AppendLine("#----------------------------- Global Properties ----------------------------#");
-stringBuilder.AppendLine("/outputDir:bin/$(Platform)");
-stringBuilder.AppendLine("/intermediateDir:obj/$(Platform)");
-stringBuilder.AppendLine("/platform:DesktopGL");
-stringBuilder.AppendLine("/config:");
-stringBuilder.AppendLine("/profile:Reach");
-stringBuilder.AppendLine("/compress:False");
-stringBuilder.AppendLine("");
+contentBuilder.AppendLine("#----------------------------- Global Properties ----------------------------#");
+contentBuilder.AppendLine("/outputDir:bin/$(Platform)");
+contentBuilder.AppendLine("/intermediateDir:obj/$(Platform)");
+contentBuilder.AppendLine("/platform:DesktopGL");
+contentBuilder.AppendLine("/config:");
+contentBuilder.AppendLine("/profile:Reach");
+contentBuilder.AppendLine("/compress:False");
+contentBuilder.AppendLine("");
 
-stringBuilder.AppendLine("#-------------------------------- References --------------------------------#");
-stringBuilder.AppendLine("");
+contentBuilder.AppendLine("#-------------------------------- References --------------------------------#");
+contentBuilder.AppendLine("");
 
-stringBuilder.AppendLine("#---------------------------------- Content ---------------------------------#");
-stringBuilder.AppendLine("");
+contentBuilder.AppendLine("#---------------------------------- Content ---------------------------------#");
+contentBuilder.AppendLine("");
 void AppendCopyFile(string filePath)
 {
     string relativePath = Path.GetRelativePath(executingDirectory, filePath);
-    stringBuilder.AppendLine($"#begin {relativePath}");
-    stringBuilder.AppendLine($"/copy:{relativePath}");
+    contentBuilder.AppendLine($"#begin {relativePath}");
+    contentBuilder.AppendLine($"/copy:{relativePath}");
+    contentBuilder.AppendLine("");
 }
 
 void AppendSpriteFont(string filePath)
 {
     string relativePath = Path.GetRelativePath(executingDirectory, filePath);
-    stringBuilder.AppendLine($"#begin {relativePath}");
-    stringBuilder.AppendLine($"/importer:FontDescriptionImporter");
-    stringBuilder.AppendLine($"/processor:FontDescriptionProcessor");
-    stringBuilder.AppendLine($"/processorParam:PremultiplyAlpha=True");
-    stringBuilder.AppendLine($"/processorParam:TextureFormat=Compressed");
-    stringBuilder.AppendLine($"/build:{relativePath}");
+    contentBuilder.AppendLine($"#begin {relativePath}");
+    contentBuilder.AppendLine($"/importer:FontDescriptionImporter");
+    contentBuilder.AppendLine($"/processor:FontDescriptionProcessor");
+    contentBuilder.AppendLine($"/processorParam:PremultiplyAlpha=True");
+    contentBuilder.AppendLine($"/processorParam:TextureFormat=Compressed");
+    contentBuilder.AppendLine($"/build:{relativePath}");
+    contentBuilder.AppendLine("");
 }
 
 void AppendOGG(string filePath)
 {
     string relativePath = Path.GetRelativePath(executingDirectory, filePath);
-    stringBuilder.AppendLine($"#begin {relativePath}");
-    stringBuilder.AppendLine($"/importer:OggImporter");
-    stringBuilder.AppendLine($"/processor:SoundEffectProcessor");
-    stringBuilder.AppendLine($"/processorParam:Quality=Best");
-    stringBuilder.AppendLine($"/build:{relativePath}");
+    contentBuilder.AppendLine($"#begin {relativePath}");
+    contentBuilder.AppendLine($"/importer:OggImporter");
+    contentBuilder.AppendLine($"/processor:SoundEffectProcessor");
+    contentBuilder.AppendLine($"/processorParam:Quality=Best");
+    contentBuilder.AppendLine($"/build:{relativePath}");
+    contentBuilder.AppendLine("");
 }
 
 void ParseDirectory(string directory)
@@ -69,11 +74,14 @@ void ParseDirectory(string directory)
     string[] fileNames = Directory.GetFiles(directory);
     foreach (string file in fileNames)
     {
+        if (file.Contains("bin"))
+            continue;
+
         FileInfo fileInfo = new FileInfo(file);
         Console.WriteLine(fileInfo.Extension);
         switch (fileInfo.Extension)
         {
-            case ".json":
+            case ".hjson":
             case ".dat":
             case ".aseprite":
                 AppendCopyFile(file);
@@ -89,6 +97,7 @@ void ParseDirectory(string directory)
 }
 
 ParseDirectory(executingDirectory);
-Console.WriteLine(stringBuilder.ToString());
-File.WriteAllText(expectedFilePath, stringBuilder.ToString()); 
+Console.WriteLine(contentBuilder.ToString());
+File.WriteAllText(expectedFilePath, contentBuilder.ToString()); 
 Console.WriteLine("Successfully written changes to the content.mgcb file");
+Console.ReadLine();
