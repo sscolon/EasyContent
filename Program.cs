4dc4f5f3f0cd15f410e784dde63f2daf9fef08bc
Scolon <scoloned@gmail.com>
2026-02-04 04:02:04 -0500

    i should really make a config file alr

--- Program.cs
+++ Program.cs
@@ -32,6 +32,69 @@ contentBuilder.AppendLine("");
 
 contentBuilder.AppendLine("#---------------------------------- Content ---------------------------------#");
 contentBuilder.AppendLine("");
+
+void AppendFX(string filePath)
+{
+    string relativePath = Path.GetRelativePath(executingDirectory, filePath);
+    contentBuilder.AppendLine($"#begin {relativePath}");
+    contentBuilder.AppendLine($"/importer:EffectImporter");
+    contentBuilder.AppendLine($"/processor:EffectProcessor");
+    contentBuilder.AppendLine($"/processorParam:DebugMode=Auto");
+    contentBuilder.AppendLine($"/build:{relativePath}");
+    contentBuilder.AppendLine("");
+    /*
+     * #begin Shaders/abyssocean.fx
+/importer:EffectImporter
+/processor:EffectProcessor
+/processorParam:DebugMode=Auto
+/build:Shaders/abyssocean.fx
+     */
+}
+
+void AppendFBX(string filePath)
+{
+    /*
+     * #begin Skies/SkySphere.fbx
+        /importer:FbxImporter
+        /processor:ModelProcessor
+        /processorParam:ColorKeyColor=0, 0, 0, 0
+        /processorParam:ColorKeyEnabled=True
+        /processorParam:DefaultEffect=BasicEffect
+        /processorParam:GenerateMipmaps=True
+        /processorParam:GenerateNormals=False
+        /processorParam:GenerateTangentFrames=False
+        /processorParam:PremultiplyTextureAlpha=True
+        /processorParam:PremultiplyVertexColors=True
+        /processorParam:ResizeTexturesToPowerOfTwo=False
+        /processorParam:RotationX=0
+        /processorParam:RotationY=0
+        /processorParam:RotationZ=0
+        /processorParam:Scale=1
+        /processorParam:SwapWindingOrder=False
+        /processorParam:TextureFormat=Compressed
+        /build:Skies/SkySphere.fbx
+     */
+    string relativePath = Path.GetRelativePath(executingDirectory, filePath);
+    contentBuilder.AppendLine($"#begin {relativePath}");
+    contentBuilder.AppendLine("/processor:ModelProcessor");
+    contentBuilder.AppendLine("/processorParam:ColorKeyColor=0, 0, 0, 0");
+    contentBuilder.AppendLine("/processorParam:ColorKeyEnabled=True");
+    contentBuilder.AppendLine("/processorParam:DefaultEffect=BasicEffect");
+    contentBuilder.AppendLine("/processorParam:GenerateMipmaps=True");
+    contentBuilder.AppendLine("/processorParam:GenerateNormals=False");
+    contentBuilder.AppendLine("/processorParam:GenerateTangentFrames=False");
+    contentBuilder.AppendLine("/processorParam:PremultiplyTextureAlpha=True");
+    contentBuilder.AppendLine("/processorParam:PremultiplyVertexColors=True");
+    contentBuilder.AppendLine("/processorParam:ResizeTexturesToPowerOfTwo=False");
+    contentBuilder.AppendLine("/processorParam:RotationX=0");
+    contentBuilder.AppendLine("/processorParam:RotationY=0");
+    contentBuilder.AppendLine("/processorParam:RotationZ=0");
+    contentBuilder.AppendLine("/processorParam:Scale=1");
+    contentBuilder.AppendLine("/processorParam:SwapWindingOrder=False");
+    contentBuilder.AppendLine("/processorParam:TextureFormat=Compressed");
+    contentBuilder.AppendLine($"/build:{relativePath}");
+    contentBuilder.AppendLine("");
+}
 void AppendCopyFile(string filePath)
 {
     string relativePath = Path.GetRelativePath(executingDirectory, filePath);
@@ -81,10 +144,19 @@ void ParseDirectory(string directory)
         Console.WriteLine(fileInfo.Extension);
         switch (fileInfo.Extension)
         {
+            case ".pal":
+            case ".png":
             case ".hjson":
             case ".dat":
+            case ".lvl":
             case ".aseprite":
                 AppendCopyFile(file);
+                break;
+            case ".fbx":
+                AppendFBX(file);
+                break;
+            case ".fx":
+                AppendFX(file);
                 break;
             case ".ogg":
                 AppendOGG(file);
@@ -100,4 +172,4 @@ ParseDirectory(executingDirectory);
 Console.WriteLine(contentBuilder.ToString());
 File.WriteAllText(expectedFilePath, contentBuilder.ToString()); 
 Console.WriteLine("Successfully written changes to the content.mgcb file");
-Console.ReadLine();
+//Console.ReadLine();
